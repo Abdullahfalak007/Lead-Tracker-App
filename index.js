98f9e9fe75c1d44e374bc43720b76ccc2a81310c
Muhammad Abdullah <93870726+Abdullahfalak007@users.noreply.github.com>
2024-01-28 04:18:44 +0500

    Version 2.0

--- index.js
+++ index.js
@@ -5,17 +5,13 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    //to clear out he input field
     inputEl.value = ""
-    // 2. Call the renderLeads() function
     renderLeads()
 })
 
-// 1. Wrap the code below in a renderLeads() function
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
@@ -24,16 +20,5 @@ function renderLeads() {
             </li>
         `
     }
-    }
     ulEl.innerHTML = listItems  
 }
-
-// let listItems = ""
-// for (let i = 0; i < myLeads.length; i++) {
-//     // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
-//     listItems += "<li>" + myLeads[i] + "</li>"
-    
-// }
-// // 3. Render the listItems inside the unordered list using ulEl.innerHTML
-// ulEl.innerHTML = listItems
-
